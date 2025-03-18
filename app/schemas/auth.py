550752964a4b53d53652bae0494c2571dc88fba0
Daniel Tilahun <tiledan2015@gmail.com>
2025-03-18 23:32:41 +0300

    schemas is added

---
+++ app/schemas/auth.py
@@ -0,0 +1,46 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+from typing import List
+
+
+class BaseConfig:
+    from_attributes = True
+
+
+class UserBase(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+    full_name: str
+    password: str
+    role: str
+    is_active: bool
+    created_at: datetime
+
+    class Config(BaseConfig):
+        pass
+
+
+class Signup(BaseModel):
+    full_name: str
+    username: str
+    email: str
+    password: str
+
+    class Config(BaseConfig):
+        pass
+
+
+class UserOut(BaseModel):
+    message: str
+    data: UserBase
+
+    class Config(BaseConfig):
+        pass
+
+
+class TokenResponse(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = 'Bearer'
+    expires_in: int
