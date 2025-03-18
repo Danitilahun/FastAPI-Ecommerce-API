a1994bd62a65626a948be83c4c33f3650397f538
Daniel Tilahun <tiledan2015@gmail.com>
2025-03-18 19:06:18 +0300

    feat:basic app setup done

---
+++ app/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI , APIRouter
+
+test_router = APIRouter()
+
+@test_router.get("/test")
+async def test_endpoint():
+    return {"message": "Test endpoint works!"}
+
+# --- App Description ---
+description = """
+    Welcome to the E-commerce API! 🚀
+
+    This API will be used to demo how to use pytest and GitHub Actions for CI/CD.
+
+    Done by Group 8 for a Selected Topic Assignment.
+"""
+
+# --- FastAPI App ---
+app = FastAPI(
+    description=description,
+    title="E-commerce API",
+    version="1.0.0",
+    swagger_ui_parameters={
+        "syntaxHighlight.theme": "monokai",
+        "layout": "BaseLayout",
+        "filter": True,
+        "tryItOutEnabled": True,
+        "onComplete": "Ok"
+    },
+)
+
+app.include_router(test_router)
