953c0400d92c3c0b856d7bbf1fc2ee91aa8976f8
Daniel Tilahun <tiledan2015@gmail.com>
2025-03-19 17:49:15 +0300

    add_orders_and_order_items_tables

--- app/models/models.py
+++ app/models/models.py
@@ -1,4 +1,7 @@
-from sqlalchemy import TIMESTAMP, Column, Enum, Integer, String, Boolean, text
+from sqlalchemy import Boolean, Column, Integer, String, ForeignKey, Float, ARRAY, Enum
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.orm import relationship
 from app.db.database import Base
 
 
@@ -13,3 +16,68 @@ class User(Base):
     is_active = Column(Boolean, server_default="True", nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), server_default=text("NOW()"), nullable=False)
     role = Column(Enum("admin", "user", name="user_roles"), nullable=False, server_default="user")
+
+    # Relationship with orders
+    orders = relationship("Order", back_populates="user")
+
+
+class Order(Base):
+    __tablename__ = "orders"
+
+    id = Column(Integer, primary_key=True, nullable=False, unique=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=text("NOW()"), nullable=False)
+    total_amount = Column(Float, nullable=False)
+
+    # Relationship with user
+    user = relationship("User", back_populates="orders")
+
+    # Relationship with order items
+    order_items = relationship("OrderItem", back_populates="order")
+
+
+class OrderItem(Base):
+    __tablename__ = "order_items"
+
+    id = Column(Integer, primary_key=True, nullable=False, unique=True, autoincrement=True)
+    order_id = Column(Integer, ForeignKey("orders.id", ondelete="CASCADE"), nullable=False)
+    product_id = Column(Integer, ForeignKey("products.id", ondelete="CASCADE"), nullable=False)
+    quantity = Column(Integer, nullable=False)
+    subtotal = Column(Float, nullable=False)
+
+    # Relationship with order and product
+    order = relationship("Order", back_populates="order_items")
+    product = relationship("Product", back_populates="order_items")
+
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id = Column(Integer, primary_key=True, nullable=False, unique=True, autoincrement=True)
+    name = Column(String, unique=True, nullable=False)
+
+    products = relationship("Product", back_populates="category")
+
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, nullable=False, unique=True, autoincrement=True)
+    title = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    price = Column(Integer, nullable=False)
+    discount_percentage = Column(Float, nullable=False)
+    rating = Column(Float, nullable=False)
+    stock = Column(Integer, nullable=False)
+    brand = Column(String, nullable=False)
+    thumbnail = Column(String, nullable=False)
+    images = Column(ARRAY(String), nullable=False)
+    is_published = Column(Boolean, server_default="True", nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=text("NOW()"), nullable=False)
+
+    # Relationship with category
+    category_id = Column(Integer, ForeignKey("categories.id", ondelete="CASCADE"), nullable=False)
+    category = relationship("Category", back_populates="products")
+
+    # Relationship with order items
+    order_items = relationship("OrderItem", back_populates="product")
